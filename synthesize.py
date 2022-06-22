aced8e50c594246238576c8378c2c98a1f44d3dc
Ali Mohammed <ali.mohammad@infostrategic.com>
2022-06-22 16:38:50 +0300

    upload dataset

--- synthesize.py
+++ synthesize.py
@@ -1,42 +1,40 @@
-import ruamel.yaml
+import os
 from model.models import ForwardTransformer
-from pathlib import Path
 from data.audio import Audio
 from vocoding.predictors import HiFiGANPredictor
-
-yaml = ruamel.yaml.YAML()
+from argparse import ArgumentParser
 
 
 def synthesize(text):
     if not text:
         return None
-    wav_no_vc_path = Path("./output/wav_no_vc")
-    wave_vc_path = Path("./output/wav_vc")
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    vocoder = HiFiGANPredictor.from_folder('vocoding/hifigan/custom/')
-    # with open("./step_95000/config.yaml", 'rb') as session_yaml:
-    #     config = yaml.load(session_yaml)
-    # model = ForwardTransformer.from_config(config)
-    model = ForwardTransformer.load_model('./step_95000')
-    # audio = Audio(config=config)
+    model_path = os.path.join(dir_path, "tts_model")
+    vocoder_path = os.path.join(dir_path, "vocoding", "hifigan", "custom")
+
+    wav_no_vc_path = os.path.join(dir_path, "output", "wav_no_vc.wav")
+    wave_vc_path = os.path.join(dir_path, "output", "wav_vc.wav")
+
+    model = ForwardTransformer.load_model(model_path)
+    vocoder = HiFiGANPredictor.from_folder(vocoder_path)
     audio = Audio.from_config(model.config)
+
     out = model.predict(text, speed_regulator=1.)
 
-    wav_no_vc = audio.reconstruct_waveform(out['mel'].numpy().T)
     wav_vc = vocoder([out['mel'].numpy().T])[0]
 
-    audio.save_wav(wav_no_vc, wav_path=(wav_no_vc_path).with_suffix('.wav'))
-    audio.save_wav(wav_vc, wav_path=(wave_vc_path).with_suffix('.wav'))
+    audio.save_wav(wav_vc, wav_path=wave_vc_path)
+    return wave_vc_path
 
 
-synthesize("لَقَدْ قَدِمْتُ الْبَصْرَةَ، فَأَصَبْتَ آلَافًا فَمَا اكْتَرَثْتُ بِهَا فَرَحًا، وَلا حَدَّثْتُ نَفْسِي بِالْكُرْهِ أَيْضًا")
-
-
-# ثُمَّ مَضَى إِلَى الْيَمَنِ فَقَتَلَ بِهَا ابْنَيْ عُبَيْدِ اللَّهِ بْنِ عَبَّاسٍ، صَبِيَّيْنِ مُلَيْحَيْنِ، فَهَامَتْ أُمُّهُمَا بِهِمَا
-# ضَرَبَ الزُّبَيْرُ أَسْمَاءَ، فَصَاحَتْ لِعَبْدِ اللَّهِ بْنِ الزُّبَيْرِ، فَأَقْبَلَ، فَلَمَّا رَآهُ، قَالَ: أُمَّكَ طَالِقٌ إِنْ دَخَلَتْ
-# آخِرُ الْمُهَاجِرِينَ وَالْمُهَاجِرَاتِ وَفَاةً، وَأُمُّهَا قُتَيْلَةُ بِنْتُ عَبْدِ الْعُزَّى الْعَامِرِيَّةُ، لَهَا عِدَّةُ أَحَادِيثَ.
-# فَأَرْسَلَ إِلَيْهَا لَتَأْتِيَنَّ أَوْ لَأَبْعَثَنَّ مَنْ يَسْحَبُكِ بِقُرُونِكِ
-# وَقُتِلَ شُرَيْحُ بْنُ هَانِئٍ الْحَارِثِيُّ، وَأَصَابَ الْعَسْكَرَ ضِيقٌ وَجُوعٌ شَدِيدٌ، حَتَّى هَلَكَ عَامَّتُهُمْ
-# لِأَنَّهُ يَتَلَذَّذُ بِأَنْوَاعِ الْمَأْكَلِ وَالْمَشْرَبِ وَغَيْرِ ذَلِكَ، أَيْ أَغْلَبُ أَفْرَادِهِ فَلَا يَرِدُ الْمَرِيضُ الَّذِي أَضْنَاهُ الْمَرَضُ فَصَارَ لَا يَقْدِرُ عَلَى تَنَاوُلِ مَا فِيهِ لَذَّةٌ أَوْ كُلُّ أَفْرَادِهِ، وَنَقُولُ: الْكَافِرُ الْمَذْكُورُ بِاعْتِبَارِ مَا يَعْقُبُهُ مِنْ أَنْوَاعِ الْأَلَمِ فِي الْآخِرَةِ.
-# لَقَدْ قَدِمْتُ الْبَصْرَةَ، فَأَصَبْتَ آلَافًا فَمَا اكْتَرَثْتُ بِهَا فَرَحًا، وَلا حَدَّثْتُ نَفْسِي بِالْكُرْهِ أَيْضًا
-# إِنِّي سَمِعْتُ أَبَا الدَّرْدَاءِ، يَقُولُ: إِنَّ ذَا الدِّرْهَمَيْنِ يَوْمَ الْقِيَامَةِ أَشَدُّ حِسَابًا مِنْ ذِي الدِّرْهَمِ
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--text', '-t', dest='text', default=None, type=str)
+    args = parser.parse_args()
+    if args.text is None:
+        print(" ################ ")
+        print("ُError: no text entered")
+        print("################")
+        exit(0)
+    synthesize(args.text)
